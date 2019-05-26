0d3a81c1284dafae3026a13e874346e4acf8bd82
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-26 09:28:20 +0530

    Add meetup gri comp

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import Header from "./UI/Header.svelte";
-  import MeetupItem from "./Meetups/MeetupItem.svelte";
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
 
   const meetups = [
     {
@@ -27,21 +27,13 @@
 </script>
 
 <style>
-  .meetups {
+  main {
     margin-top: 5rem;
   }
 </style>
 
 <Header />
 
-<section class="meetups">
-  {#each meetups as meetup (meetup.id)}
-    <MeetupItem
-      title={meetup.title}
-      subtitle={meetup.subtitle}
-      imageUrl={meetup.imageUrl}
-      description={meetup.description}
-      address={meetup.address}
-      email={meetup.contactEmail} />
-  {/each}
-</section>
+<main>
+  <MeetupGrid {meetups} />
+</main>
---
+++ src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,32 @@
+<script>
+  import MeetupItem from "./MeetupItem.svelte";
+
+  export let meetups;
+</script>
+
+<style>
+  section {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    section {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+</style>
+
+<section>
+  {#each meetups as meetup (meetup.id)}
+    <MeetupItem
+      title={meetup.title}
+      subtitle={meetup.subtitle}
+      imageUrl={meetup.imageUrl}
+      description={meetup.description}
+      address={meetup.address}
+      email={meetup.contactEmail} />
+  {/each}
+</section>
