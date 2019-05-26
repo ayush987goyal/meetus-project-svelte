f1db7f36112ad438b0731b5c3246f05acb7111f8
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-26 20:28:04 +0530

    Add badge comp with solt

--- src/Meetups/MeetupItem.svelte
+++ src/Meetups/MeetupItem.svelte
@@ -2,6 +2,7 @@
   import { createEventDispatcher } from "svelte";
 
   import Button from "../UI/Button.svelte";
+  import Badge from "../UI/Badge.svelte";
 
   export let id;
   export let title;
@@ -75,7 +76,12 @@
 
 <article>
   <header>
-    <h1>{title}</h1>
+    <h1>
+       {title}
+      {#if isFav}
+        <Badge>FAVORITE</Badge>
+      {/if}
+    </h1>
     <h2>{subtitle}</h2>
     <p>{address}</p>
   </header>
@@ -91,6 +97,7 @@
     <Button href="mailto:{email}" caption="Contact" />
     <Button
       mode="outline"
+      color={isFav ? null : 'success'}
       type="button"
       caption={isFav ? 'Unfavorite' : 'Favorite'}
       on:click={() => dispatch('togglefavorite', id)} />
