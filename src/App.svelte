93008a319f0a069652d3aea2c6ad55021a4ae1cf
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-26 20:59:19 +0530

    Add edit meeup comp

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,8 @@
 <script>
   import Header from "./UI/Header.svelte";
-  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
-  import TextInput from "./UI/TextInput.svelte";
   import Button from "./UI/Button.svelte";
-
-  let title = "";
-  let subtitle = "";
-  let imageUrl = "";
-  let email = "";
-  let address = "";
-  let description = "";
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+  import EditMeetup from "./Meetups/EditMeetup.svelte";
 
   let meetups = [
     {
@@ -35,6 +28,8 @@
       isFavorite: false
     }
   ];
+
+  let editMode;
 
   function addMeetup() {
     const newMeetup = {
@@ -65,54 +60,14 @@
   main {
     margin-top: 5rem;
   }
-
-  form {
-    width: 30rem;
-    max-width: 90%;
-    margin: auto;
-  }
 </style>
 
 <Header />
 
 <main>
-  <form on:submit|preventDefault={addMeetup}>
-    <TextInput
-      id="title"
-      label="Title"
-      value={title}
-      on:input={event => (title = event.target.value)} />
-    <TextInput
-      id="subtitle"
-      label="Subtitle"
-      value={subtitle}
-      on:input={event => (subtitle = event.target.value)} />
-    <TextInput
-      id="address"
-      label="Address"
-      value={address}
-      on:input={event => (address = event.target.value)} />
-    <TextInput
-      id="imageUrl"
-      label="Image URL"
-      value={imageUrl}
-      on:input={event => (imageUrl = event.target.value)} />
-    <TextInput
-      id="email"
-      label="E-Mail"
-      type="email"
-      value={email}
-      on:input={event => (email = event.target.value)} />
-    <TextInput
-      controlType="textarea"
-      rows="3"
-      id="description"
-      label="Description"
-      value={description}
-      on:input={event => (description = event.target.value)} />
-
-    <Button type="submit" caption="Save" />
-  </form>
-
+  <Button caption="New Meetup" on:click={() => (editMode = 'add')} />
+  {#if editMode === 'add'}
+    <EditMeetup />
+  {/if}
   <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
 </main>
---
+++ src/Meetups/EditMeetup.svelte
@@ -0,0 +1,72 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  import TextInput from "../UI/TextInput.svelte";
+  import Button from "../UI/Button.svelte";
+
+  const dispatch = createEventDispatcher();
+
+  let title = "";
+  let subtitle = "";
+  let imageUrl = "";
+  let email = "";
+  let address = "";
+  let description = "";
+
+  function submitForm() {
+    dispatch("save", {
+      title,
+      subtitle,
+      imageUrl,
+      email,
+      address,
+      description
+    });
+  }
+</script>
+
+<style>
+  form {
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
+  }
+</style>
+
+<form on:submit|preventDefault={submitForm}>
+  <TextInput
+    id="title"
+    label="Title"
+    value={title}
+    on:input={event => (title = event.target.value)} />
+  <TextInput
+    id="subtitle"
+    label="Subtitle"
+    value={subtitle}
+    on:input={event => (subtitle = event.target.value)} />
+  <TextInput
+    id="address"
+    label="Address"
+    value={address}
+    on:input={event => (address = event.target.value)} />
+  <TextInput
+    id="imageUrl"
+    label="Image URL"
+    value={imageUrl}
+    on:input={event => (imageUrl = event.target.value)} />
+  <TextInput
+    id="email"
+    label="E-Mail"
+    type="email"
+    value={email}
+    on:input={event => (email = event.target.value)} />
+  <TextInput
+    controlType="textarea"
+    rows="3"
+    id="description"
+    label="Description"
+    value={description}
+    on:input={event => (description = event.target.value)} />
+
+  <Button type="submit" caption="Save" />
+</form>
