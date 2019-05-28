fcf3751f987fd1333355edf7a142bb127d5c4265
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-28 06:24:14 +0530

    Add basic store

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,9 @@
 <script>
+  import meetups from "./Meetups/meetups-store.js";
   import Header from "./UI/Header.svelte";
   import Button from "./UI/Button.svelte";
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
   import EditMeetup from "./Meetups/EditMeetup.svelte";
-
-  let meetups = [
-    {
-      id: "m1",
-      title: "Coding Boootcamp",
-      subtitle: "Learn code in 2 hours",
-      description: "In this meetup, we will teach you coding!",
-      imageUrl:
-        "https://eventnook.s3.amazonaws.com/u/62366/coverimage_1508244245876_fe_dev_crashcourse_eventnook.png",
-      address: "11th Nerd Road, 24432 New York",
-      contactEmail: "code@test.com",
-      isFavorite: false
-    },
-    {
-      id: "m2",
-      title: "Swim Together",
-      subtitle: `Let's go for some swimming!`,
-      description: "We will simple swi some rounds.",
-      imageUrl:
-        "https://himanshudixit.me/blog/sites/default/files/inline-images/Google-Summer-of-Code-March-3-2017-800x450%20%283%29_0_0.png",
-      address: "11th Nerd Road, 24432 New York",
-      contactEmail: "swim@test.com",
-      isFavorite: false
-    }
-  ];
 
   let editMode;
 
@@ -42,7 +18,7 @@
       contactEmail: event.detail.email
     };
 
-    meetups = [newMeetup, ...meetups];
+    // meetups = [newMeetup, ...meetups];
     editMode = null;
   }
 
@@ -53,11 +29,11 @@
   function toggleFavorite(event) {
     const id = event.detail;
 
-    meetups = meetups.map(m => {
-      if (m.id !== id) return m;
+    // meetups = meetups.map(m => {
+    //   if (m.id !== id) return m;
 
-      return { ...m, isFavorite: !m.isFavorite };
-    });
+    //   return { ...m, isFavorite: !m.isFavorite };
+    // });
   }
 </script>
 
@@ -80,5 +56,5 @@
   {#if editMode === 'add'}
     <EditMeetup on:save={addMeetup} on:cancel={cancelEdit} />
   {/if}
-  <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
+  <MeetupGrid meetups={$meetups} on:togglefavorite={toggleFavorite} />
 </main>
---
+++ src/Meetups/meetups-store.js
@@ -0,0 +1,28 @@
+import { writable } from 'svelte/store';
+
+const meetups = writable([
+  {
+    id: 'm1',
+    title: 'Coding Boootcamp',
+    subtitle: 'Learn code in 2 hours',
+    description: 'In this meetup, we will teach you coding!',
+    imageUrl:
+      'https://eventnook.s3.amazonaws.com/u/62366/coverimage_1508244245876_fe_dev_crashcourse_eventnook.png',
+    address: '11th Nerd Road, 24432 New York',
+    contactEmail: 'code@test.com',
+    isFavorite: false
+  },
+  {
+    id: 'm2',
+    title: 'Swim Together',
+    subtitle: `Let's go for some swimming!`,
+    description: 'We will simple swi some rounds.',
+    imageUrl:
+      'https://himanshudixit.me/blog/sites/default/files/inline-images/Google-Summer-of-Code-March-3-2017-800x450%20%283%29_0_0.png',
+    address: '11th Nerd Road, 24432 New York',
+    contactEmail: 'swim@test.com',
+    isFavorite: false
+  }
+]);
+
+export default meetups;
