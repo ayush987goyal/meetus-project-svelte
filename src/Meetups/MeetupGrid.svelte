aeb33d6a6a9f72830e460606ea123082a442bbfb
Ayush Goyal <ayush987goyal@gmail.com>
2019-05-28 07:25:57 +0530

    Add filter com and func

--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,26 +1,43 @@
 <script>
   import MeetupItem from "./MeetupItem.svelte";
+  import MeetupFilter from "./MeetupFilter.svelte";
 
   export let meetups;
+
+  let favsOnly = false;
+
+  $: filteredMeetups = favsOnly ? meetups.filter(m => m.isFavorite) : meetups;
+
+  function setFilter(event) {
+    favsOnly = event.detail === 1;
+  }
 </script>
 
 <style>
-  section {
+  .meetups {
     width: 100%;
     display: grid;
     grid-template-columns: 1fr;
     grid-gap: 1rem;
   }
 
+  .meetup-controls {
+    margin: 1rem;
+  }
+
   @media (min-width: 768px) {
-    section {
+    .meetups {
       grid-template-columns: repeat(2, 1fr);
     }
   }
 </style>
 
-<section>
-  {#each meetups as meetup (meetup.id)}
+<section class="meetup-controls">
+  <MeetupFilter on:select={setFilter} />
+</section>
+
+<section class="meetups">
+  {#each filteredMeetups as meetup (meetup.id)}
     <MeetupItem
       id={meetup.id}
       title={meetup.title}
